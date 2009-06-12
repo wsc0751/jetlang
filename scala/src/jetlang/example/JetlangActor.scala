e1fd45c1c90d5819bcc4f84fa058b78b880b5a65
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2009-06-12 14:32:12 +0000

    support nesting.

--- scala/src/jetlang/example/JetlangActor.scala
+++ scala/src/jetlang/example/JetlangActor.scala
@@ -5,17 +5,26 @@ import org.jetlang.fibers.ThreadFiber
 
 trait JetlangActor {
   val fiber = new ThreadFiber()
-  val target = react()
+  val root = react();
+  var target = root
 
   def react(): PartialFunction[Any, Unit]
 
+  def react(newReact: PartialFunction[Any, Unit]): Unit = {
+    target = newReact
+  }
+
   def !(msg: Any): Unit = {
-    if (target.isDefinedAt(msg)) {
-      val runner = new Runnable() {
-        def run() = target(msg)
+    val runner = new Runnable() {
+      def run() = {
+        if (target.isDefinedAt(msg)) {
+          val current = target;
+          target = root;
+          current(msg)
+        }
       }
-      fiber.execute(runner)
     }
+    fiber.execute(runner)
   }
 
   def start() = fiber.start
---
+++ scala/tests/NestedReactTest.scala
@@ -0,0 +1,36 @@
+import java.util.concurrent.{TimeUnit, CountDownLatch}
+import jetlang.example.JetlangActor
+import org.scalatest.FunSuite
+
+case class A()
+case class B()
+case class C()
+
+class NestedActor extends JetlangActor {
+  val latch = new CountDownLatch(1)
+
+  def react() = {
+    case A => {
+      react {
+        case B => {
+          react {
+            case C => latch.countDown
+          }
+        }
+      }
+    }
+  }
+}
+
+class NestedReactTest extends FunSuite {
+  test("should handleNestedReacts") {
+    val actor = new NestedActor()
+    actor.start
+    actor ! A
+    actor ! B
+    actor ! C
+
+    assert(actor.latch.await(10, TimeUnit.SECONDS))
+    actor.exit
+  }
+}
