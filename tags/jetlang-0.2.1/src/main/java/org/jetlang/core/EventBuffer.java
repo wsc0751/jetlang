9fac82fa21e85722ca2deec34637e9abf7f7bb22
peter.royal@pobox.com <peter.royal@pobox.com@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2010-04-05 19:11:17 +0000

    [maven-release-plugin]  copy for tag jetlang-0.2.1

---
+++ tags/jetlang-0.2.1/src/main/java/org/jetlang/core/EventBuffer.java
@@ -0,0 +1,10 @@
+package org.jetlang.core;
+
+
+/**
+ * User: mrettig
+ * Date: Aug 29, 2009
+ */
+public class EventBuffer extends MessageBuffer<Runnable> implements EventReader {
+
+}
---
+++ tags/jetlang-0.2.1/src/main/java/org/jetlang/core/EventReader.java
@@ -0,0 +1,8 @@
+package org.jetlang.core;
+
+/**
+ * User: mrettig
+ * Date: Aug 30, 2009
+ */
+public interface EventReader extends MessageReader<Runnable> {
+}
