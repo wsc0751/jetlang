aed8c2a3d9b7d10ddcb7c2affd670882e327e808
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2011-03-09 17:03:04 +0000

    [maven-release-plugin]  copy for tag jetlang-0.2.5

---
+++ tags/jetlang-0.2.5/src/test/java/org/jetlang/tests/FiberBaseTest.java
@@ -0,0 +1,182 @@
+package org.jetlang.tests;
+
+import junit.framework.Assert;
+import org.jetlang.channels.MemoryChannel;
+import org.jetlang.core.Callback;
+import org.jetlang.core.Disposable;
+import org.jetlang.fibers.Fiber;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class FiberBaseTest extends Assert {
+    public abstract Fiber createFiber();
+
+    public abstract void doSetup();
+
+    public abstract void doTearDown();
+
+    protected Fiber _bus;
+
+    @Before
+    public void Setup() {
+        doSetup();
+        _bus = createFiber();
+    }
+
+    @After
+    public void TearDown() {
+        if (_bus != null) {
+            _bus.dispose();
+            assertEquals(0, _bus.size());
+        }
+        doTearDown();
+    }
+
+    @Test
+    public void ScheduleBeforeStart() throws InterruptedException {
+        final CountDownLatch reset = new CountDownLatch(1);
+
+        Runnable onReset = new Runnable() {
+            public void run() {
+                reset.countDown();
+            }
+        };
+        _bus.schedule(onReset, 1, TimeUnit.MILLISECONDS);
+        _bus.start();
+
+        assertTrue(reset.await(10, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void ScheduleAndCancelBeforeStart() throws InterruptedException {
+        final CountDownLatch reset = new CountDownLatch(1);
+        final AtomicBoolean executed = new AtomicBoolean();
+        Runnable toCancel = new Runnable() {
+            public void run() {
+                executed.set(true);
+            }
+        };
+        Disposable control = _bus.schedule(toCancel, 0, TimeUnit.MILLISECONDS);
+        Runnable toRun = new Runnable() {
+            public void run() {
+                reset.countDown();
+            }
+        };
+        _bus.schedule(toRun, 0, TimeUnit.MILLISECONDS);
+        control.dispose();
+        _bus.start();
+        assertTrue(reset.await(10, TimeUnit.SECONDS));
+        assertFalse(executed.get());
+    }
+
+    @Test
+    public void ScheduleOne() throws InterruptedException {
+        final CountDownLatch reset = new CountDownLatch(1);
+        _bus.start();
+        Runnable onReset = new Runnable() {
+            public void run() {
+                reset.countDown();
+            }
+        };
+        _bus.schedule(onReset, 1, TimeUnit.MILLISECONDS);
+        assertTrue(reset.await(10, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void scheduleAtFixedRate() throws InterruptedException {
+        final CountDownLatch reset = new CountDownLatch(5);
+        _bus.start();
+        Runnable onReset = new Runnable() {
+            public void run() {
+                reset.countDown();
+            }
+        };
+        _bus.scheduleAtFixedRate(onReset, 15, 15, TimeUnit.MILLISECONDS);
+        assertTrue(reset.await(2, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void scheduleWithFixedDelay() throws InterruptedException {
+        final CountDownLatch reset = new CountDownLatch(5);
+        _bus.start();
+        Runnable onReset = new Runnable() {
+            public void run() {
+                reset.countDown();
+            }
+        };
+        _bus.scheduleWithFixedDelay(onReset, 1, 1, TimeUnit.MILLISECONDS);
+        assertTrue(reset.await(10, TimeUnit.SECONDS));
+    }
+
+
+    @Test
+    public void testDoubleStartResultsInException() {
+        _bus.start();
+        try {
+            _bus.start();
+            Assert.fail("Should not start");
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void PubSub() throws InterruptedException {
+        _bus.start();
+        MemoryChannel<String> channel = new MemoryChannel<String>();
+
+        channel.publish("hello");
+        final List<String> received = new ArrayList<String>();
+        final CountDownLatch reset = new CountDownLatch(1);
+        Callback<String> onReceive = new Callback<String>() {
+            public void onMessage(String data) {
+                received.add(data);
+                reset.countDown();
+            }
+        };
+        channel.subscribe(_bus, onReceive);
+        assertEquals(1, channel.subscriberCount());
+        channel.publish("hello");
+        assertTrue(reset.await(10, TimeUnit.SECONDS));
+        assertEquals(1, received.size());
+        assertEquals("hello", received.get(0));
+
+        channel.clearSubscribers();
+        channel.publish("hello");
+    }
+
+    @Test
+    public void UnsubOnStop() throws InterruptedException {
+        _bus.start();
+        MemoryChannel<String> channel = new MemoryChannel<String>();
+        Callback<String> onReceive = new Callback<String>() {
+            public void onMessage(String data) {
+            }
+        };
+        channel.subscribe(_bus, onReceive);
+        assertEquals(1, channel.subscriberCount());
+        _bus.dispose();
+        assertEquals(0, channel.subscriberCount());
+    }
+
+    @Test
+    public void Unsub() throws InterruptedException {
+        _bus.start();
+        MemoryChannel<String> channel = new MemoryChannel<String>();
+        Callback<String> onReceive = new Callback<String>() {
+            public void onMessage(String data) {
+            }
+        };
+        Disposable unsub = channel.subscribe(_bus, onReceive);
+        assertEquals(1, channel.subscriberCount());
+        unsub.dispose();
+        assertEquals(0, channel.subscriberCount());
+    }
+
+}
---
+++ tags/jetlang-0.2.5/src/test/java/org/jetlang/tests/PoolFiberTest.java
@@ -0,0 +1,56 @@
+package org.jetlang.tests;
+
+import org.jetlang.core.Disposable;
+import org.jetlang.fibers.Fiber;
+import org.jetlang.fibers.PoolFiberFactory;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * User: mrettig
+ * Date: Jul 23, 2008
+ * Time: 8:04:04 PM
+ */
+public class PoolFiberTest extends FiberBaseTest {
+
+    private ExecutorService _executor;
+    private PoolFiberFactory _fiberFactory;
+
+    @Override
+    public Fiber createFiber() {
+        return _fiberFactory.create();
+    }
+
+    @Override
+    public void doSetup() {
+        _executor = Executors.newCachedThreadPool();
+        _fiberFactory = new PoolFiberFactory(_executor);
+    }
+
+    @Override
+    public void doTearDown() {
+        if (_executor != null)
+            _executor.shutdown();
+        if (_fiberFactory != null) {
+            _fiberFactory.dispose();
+        }
+    }
+
+    @Test
+    public void ScheduleIntervalWithCancel() throws InterruptedException {
+        _bus.start();
+        Runnable onReset = new Runnable() {
+            public void run() {
+            }
+        };
+        Disposable stopper = _bus.scheduleAtFixedRate(onReset, 15, 15, TimeUnit.MILLISECONDS);
+        assertEquals(1, _bus.size());
+        stopper.dispose();
+        assertEquals(0, _bus.size());
+
+    }
+
+}
