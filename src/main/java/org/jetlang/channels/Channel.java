670d574db4cdb9b2ecba0ca55c567a05ae8830f1
mike.rettig <mike.rettig@9f0ef8a3-a452-0410-8815-c115573c2bd9>
2011-08-04 21:44:56 +0000

    branch for concurrency patch.

--- src/main/java/org/jetlang/channels/Channel.java
+++
@@ -1,7 +0,0 @@
-package org.jetlang.channels;
-
-/**
- * Combined suscriber and publisher interface.
- */
-public interface Channel<T> extends Subscriber<T>, Publisher<T> {
-}
--- src/main/java/org/jetlang/fibers/ExecutionState.java
+++
@@ -1,7 +0,0 @@
-package org.jetlang.fibers;
-
-enum ExecutionState {
-    Created,
-    Running,
-    Stopped
-}
